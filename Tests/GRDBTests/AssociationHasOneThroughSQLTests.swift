ca4be1c951245c46b2ae841c6f8a63e3fbc7d7bd
Gwendal Roué <gr@pierlis.com>
2018-10-10 08:35:28 +0200

    Failing tests for HasOneThroughAssociation SQL generation

--- Tests/GRDBTests/AssociationHasOneThroughSQLTests.swift
+++ Tests/GRDBTests/AssociationHasOneThroughSQLTests.swift
@@ -644,34 +644,111 @@ class AssociationHasOneThroughSQLTests: GRDBTestCase {
     }
     
     func testBelongsToBelongsToBelongsTo() throws {
-        
+        struct A: TableRecord {
+            static let b = belongsTo(B.self)
+            static let c = hasOne(B.c, through: b)
+            static let d1 = hasOne(C.d, through: c)
+            static let d2 = hasOne(B.d, through: b)
+        }
+        struct B: TableRecord {
+            static let c = belongsTo(C.self)
+            static let d = hasOne(C.d, through: c)
+        }
+        struct C: TableRecord {
+            static let d = belongsTo(D.self)
+        }
+        struct D: TableRecord {
+        }
+
+        let dbQueue = try makeDatabaseQueue()
+        try dbQueue.write { db in
+            try db.create(table: "d") { t in
+                t.autoIncrementedPrimaryKey("id")
+            }
+            try db.create(table: "c") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("dId").references("d")
+            }
+            try db.create(table: "b") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("cId").references("c")
+            }
+            try db.create(table: "a") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("bId").references("b")
+            }
+            
+            do {
+                for request in [
+                    A.including(optional: A.d1),
+                    A.including(optional: A.d2)]
+                {
+                    try assertEqualSQL(db, request, """
+                        SELECT "a".*, "d".* \
+                        FROM "a" \
+                        LEFT JOIN "b" ON ("b"."id" = "a"."bId") \
+                        LEFT JOIN "c" ON ("c"."id" = "b"."cId") \
+                        LEFT JOIN "d" ON ("d"."id" = "c"."dId")
+                        """)
+                }
+                for request in [
+                    A.including(required: A.d1),
+                    A.including(required: A.d2)]
+                {
+                    try assertEqualSQL(db, request, """
+                        SELECT "a".*, "d".* \
+                        FROM "a" \
+                        JOIN "b" ON ("b"."id" = "a"."bId") \
+                        JOIN "c" ON ("c"."id" = "b"."cId") \
+                        JOIN "d" ON ("d"."id" = "c"."dId")
+                        """)
+                }
+                for request in [
+                    A.joining(optional: A.d1),
+                    A.joining(optional: A.d2)]
+                {
+                    try assertEqualSQL(db, request, """
+                        SELECT "a".* \
+                        FROM "a" \
+                        LEFT JOIN "b" ON ("b"."id" = "a"."bId") \
+                        LEFT JOIN "c" ON ("c"."id" = "b"."cId") \
+                        LEFT JOIN "d" ON ("d"."id" = "c"."dId")
+                        """)
+                }
+                for request in [
+                    A.joining(required: A.d1),
+                    A.joining(required: A.d2)]
+                {
+                    try assertEqualSQL(db, request, """
+                        SELECT "a".* \
+                        FROM "a" \
+                        JOIN "b" ON ("b"."id" = "a"."bId") \
+                        JOIN "c" ON ("c"."id" = "b"."cId") \
+                        JOIN "d" ON ("d"."id" = "c"."dId")
+                        """)
+                }
+            }
+        }
     }
     
     func testBelongsToBelongsToHasOne() throws {
-        
     }
     
     func testBelongsToHasOneBelongsTo() throws {
-        
     }
     
     func testBelongsToHasOneHasOne() throws {
-        
     }
     
     func testHasOneBelongsToBelongsTo() throws {
-        
     }
     
     func testHasOneBelongsToHasOne() throws {
-        
     }
     
     func testHasOneHasOneBelongsTo() throws {
-        
     }
     
     func testHasOneHasOneHasOne() throws {
-        
     }
 }
